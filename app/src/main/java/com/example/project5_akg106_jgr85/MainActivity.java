18a97d04c7e6c206d2dea36f6ab5e2ab515dbbb1
JustinRhodes14 <rhodesju21@gmail.com>
2021-11-21 18:51:33 -0500

    main activity and pizza activity fully functional TODO: finish add order button, set up current order activity

--- app/src/main/java/com/example/project5_akg106_jgr85/MainActivity.java
+++ app/src/main/java/com/example/project5_akg106_jgr85/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.project5_akg106_jgr85;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+/**
+ * Main activity class for ordering pizzas, looking at orders, and viewing store orders.
+ * @author Andy Giang, Justin Rhodes
+ */
 public class MainActivity extends AppCompatActivity {
 
     public static final String PIZZA_TYPE = "com.example.project5_akg106_jgr85.MESSAGE";
@@ -14,49 +21,95 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btn1;
     private ImageButton btn2;
     private ImageButton btn3;
+    private EditText editTextPhoneNumber;
 
+    /**
+     * Creates activity for main pizza view.
+     * @param savedInstanceState Saved instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = (ImageButton) findViewById(R.id.imageButton);
+        editTextPhoneNumber = (EditText) findViewById(R.id.editTextPhoneNumber);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deluxeClick();
+                if (validPhoneNumber()) {
+                    deluxeClick();
+                } else {
+                    phoneToast();
+                }
             }
         });
         btn2 = (ImageButton) findViewById(R.id.imageButton3);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hawaiianClick();
+                if (validPhoneNumber()) {
+                    hawaiianClick();
+                } else {
+                    phoneToast();
+                }
             }
         });
         btn3 = (ImageButton) findViewById(R.id.imageButton4);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pepperoniClick();
+                if (validPhoneNumber()) {
+                    pepperoniClick();
+                } else {
+                    phoneToast();
+                }
             }
         });
     }
 
+    /**
+     * Event handler for deluxe pizza button click.
+     */
     protected void deluxeClick() {
         Intent intent = new Intent(this, PizzaActivity.class);
         intent.putExtra(PIZZA_TYPE,"Deluxe");
         startActivity(intent);
     }
 
+    /**
+     * Event handler for hawaiian pizza button click.
+     */
     protected void hawaiianClick() {
         Intent intent = new Intent(this, PizzaActivity.class);
         intent.putExtra(PIZZA_TYPE,"Hawaiian");
         startActivity(intent);
     }
 
+    /**
+     * Event handler for pepperoni pizza button click.
+     */
     protected void pepperoniClick() {
         Intent intent = new Intent(this, PizzaActivity.class);
         intent.putExtra(PIZZA_TYPE,"Pepperoni");
         startActivity(intent);
+    }
+
+    /**
+     * Ensures text phone number is a valid entry before proceeding to other activities.
+     * @return
+     */
+    protected boolean validPhoneNumber() {
+        return (editTextPhoneNumber.getText().toString().length() == 10);
+    }
+
+    /**
+     * Toast for invalid phone number entries.
+     */
+    protected void phoneToast() {
+        Context context = getApplicationContext();
+        CharSequence text = "Invalid phone number, must be 10 digits";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/project5_akg106_jgr85/PizzaActivity.java
+++ app/src/main/java/com/example/project5_akg106_jgr85/PizzaActivity.java
@@ -4,24 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import projectFiles.Deluxe;
 import projectFiles.Hawaiian;
+import projectFiles.Order;
 import projectFiles.Pepperoni;
+import projectFiles.Pizza;
+import projectFiles.Size;
 import projectFiles.Topping;
 
+/**
+ * The activity class to handle orders for specific types of pizza, updates element content appropriately for each pizza.
+ * @author Andy Giang, Justin Rhodes
+ */
 public class PizzaActivity extends AppCompatActivity {
 
-    String tempArray[] = {"Hello","Happy","Helpful","Handy"};
     private ImageView imageView;
-    private ListView listView1;
-    private ListView listView2;
+    private ListView additionalToppings;
+    private ListView selectedToppings;
+    private TextView priceText;
+    private Spinner sizeSpinner;
 
+    public static final int SMALL = 1;
+    public static final int MAX_TOPPINGS = 7;
+    public static ArrayList<Topping> DEFAULT_TOPPINGS;
+    public static final int SETUP = 1;
+    public static final int NOT_SETUP = 0;
+
+    private Order o;
+    private ArrayList<Topping> selItems;
+    private ArrayList<Topping> addItems;
+    private ArrayAdapter adapter1;
+    private ArrayAdapter adapter2;
+    private int currSize = SMALL;
+    private int initial = SETUP;
+
+    /**
+     * Creates new activity for ordering pizzas.
+     * @param savedInstanceState Saved instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,28 +60,171 @@ public class PizzaActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.PIZZA_TYPE);
+        initViews(message);
+        priceText = (TextView)findViewById(R.id.textView7);
+        priceText.setText(setPrice(message));
+        initListeners();
+        initSpinListen();
+    }
 
+    /**
+     * Initializes the spinner functionality for changing the price.
+     */
+    protected void initSpinListen() {
+        sizeSpinner = (Spinner)findViewById(R.id.spinner);
+        sizeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(currSize);
+                System.out.println(sizeSpinner.getSelectedItem());
+                incrementPrice(currSize,Size.toSize((String)(sizeSpinner.getSelectedItem())));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    /**
+     * Initializes the listviews and adapters for listviews, as well as priceText.
+     */
+    protected void initListeners() {
+        additionalToppings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (selItems.size() == MAX_TOPPINGS) {
+                    addToast();
+                    return;
+                }
+
+                selItems.add(addItems.get(position));
+                addItems.remove(position);
+                adapter1.notifyDataSetChanged();
+                adapter2.notifyDataSetChanged();
+                priceText.setText(String.format("%,.2f",(Double.parseDouble(priceText.getText().toString()) + Pizza.ADDITIONAL_TOPPING_PRICE)));
+            }
+        });
+        selectedToppings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (DEFAULT_TOPPINGS.contains(selItems.get(position))) {
+                    defaultToast();
+                    return;
+                }
+                addItems.add(selItems.get(position));
+                selItems.remove(position);
+                adapter1.notifyDataSetChanged();
+                adapter2.notifyDataSetChanged();
+                priceText.setText(String.format("%,.2f",(Double.parseDouble(priceText.getText().toString()) - Pizza.ADDITIONAL_TOPPING_PRICE)));
+            }
+        });
+    }
+
+    /**
+     * Initializes the image views of the activity, as well as the items in the listviews and default toppings.
+     * @param message Message from the intent.
+     */
+    protected void initViews(String message) {
         imageView = (ImageView)findViewById(R.id.imageView);
-        listView1 = (ListView)findViewById(R.id.listView1);
-        listView2 = (ListView)findViewById(R.id.listView2);
-        ArrayList<Topping> listItems1;
-        ArrayList<Topping> listItems2;
+        additionalToppings = (ListView)findViewById(R.id.listView2);
+        selectedToppings = (ListView)findViewById(R.id.listView1);
         if (message.equals("Deluxe")) {
             imageView.setImageResource(R.drawable.deluxepizza);
-            listItems1 = Deluxe.selectedComboBox();
-            listItems2 = Deluxe.additionalComboBox();
+            selItems = Deluxe.selectedComboBox();
+            addItems = Deluxe.additionalComboBox();
+            DEFAULT_TOPPINGS = Deluxe.selectedComboBox();
         } else if (message.equals("Hawaiian")) {
             imageView.setImageResource(R.drawable.hawaiianpizza);
-            listItems1 = Hawaiian.selectedComboBox();
-            listItems2 = Hawaiian.additionalComboBox();
+            selItems = Hawaiian.selectedComboBox();
+            addItems = Hawaiian.additionalComboBox();
+            DEFAULT_TOPPINGS = Hawaiian.selectedComboBox();
         } else {
             imageView.setImageResource(R.drawable.pepperonipizza);
-            listItems1 = Pepperoni.selectedComboBox();
-            listItems2 = Pepperoni.additionalComboBox();
+            selItems = Pepperoni.selectedComboBox();
+            addItems = Pepperoni.additionalComboBox();
+            DEFAULT_TOPPINGS = Pepperoni.selectedComboBox();
         }
-        ArrayAdapter adapter1 = new ArrayAdapter<Topping>(this,R.layout.activity_listview,listItems2);
-        ArrayAdapter adapter2 = new ArrayAdapter<Topping>(this,R.layout.activity_listview,listItems1);
-        listView2.setAdapter(adapter1);
-        listView1.setAdapter(adapter2);
+        adapter1 = new ArrayAdapter<>(this,R.layout.activity_listview,addItems);
+        adapter2 = new ArrayAdapter<>(this,R.layout.activity_listview,selItems);
+        selectedToppings.setAdapter(adapter2);
+        additionalToppings.setAdapter(adapter1);
+    }
+
+    /**
+     * Sets the price text of the activity.
+     * @param message Message from the intent.
+     * @return Price of label.
+     */
+    protected CharSequence setPrice(String message) {
+        if (message.equals("Deluxe")) {
+            return "" + Pizza.DELUXE_PRICE;
+        } else if (message.equals("Hawaiian")) {
+            return "" + Pizza.HAWAIIAN_PRICE;
+        } else {
+            return "" + Pizza.PEPPERONI_PRICE;
+        }
+    }
+
+    /**
+     * Toast for invalid addition of toppings.
+     */
+    protected void addToast() {
+        CharSequence text = "You may only have 7 toppings.";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(getBaseContext(),text,duration);
+        toast.show();
+    }
+
+    /**
+     * Toast for invalid removal of toppings.
+     */
+    protected void defaultToast() {
+        CharSequence text = "You cannot remove default toppings.";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(getBaseContext(),text,duration);
+        toast.show();
+    }
+
+    /**
+     * Increments price for when new size is selected on spinner.
+     * @param prevPrice Previous size price.
+     * @param s Size to be set to.
+     */
+    public void incrementPrice(int prevPrice, Size s) {
+        if (initial == SETUP) {
+            initial = NOT_SETUP;
+            return;
+        }
+        double currPrice = 0.0;
+        if (s == Size.Medium) {
+            if (prevPrice == Size.Small.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) + Pizza.MEDIUM;
+            }
+            else if (prevPrice == Size.Large.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) - Pizza.MEDIUM;
+            }
+            currSize = Size.Medium.getLabel();
+        }
+        else if (s == Size.Large) {
+            if (prevPrice == Size.Small.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) + Pizza.LARGE;
+            }
+            else if (prevPrice == Size.Medium.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) + Pizza.MEDIUM;
+            }
+            currSize = Size.Large.getLabel();
+        }
+        else {
+            if (prevPrice == Size.Large.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) - Pizza.LARGE;
+            }
+            else if (prevPrice == Size.Medium.getLabel()) {
+                currPrice = Double.parseDouble(priceText.getText().toString()) - Pizza.MEDIUM;
+            }
+            currSize = Size.Small.getLabel();
+        }
+        priceText.setText(String.format("%,.2f", currPrice));
     }
 }
